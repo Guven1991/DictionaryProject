a878e75478f2bf8877ccee0d2ae9443f17dba05a
Guven1991 <gayvazoglu.29@gmail.com>
2022-08-18 15:56:33 +0300

    updated

--- backend/readme.md
+++
@@ -1,6 +0,0 @@
-### It is an application that I wrote to make it easier to memorize foreign words.I read my own words from an excel file and saved them to the Database.
-
-# Technologies I use
-## Java
-## Spring Boot
-## PostgreSQL
--- backend/src/main/java/com/example/dictionaryapp/repository/WordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dictionaryapp.repository;
-
-import com.example.dictionaryapp.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordRepository extends JpaRepository<Word,Long> {
-}
